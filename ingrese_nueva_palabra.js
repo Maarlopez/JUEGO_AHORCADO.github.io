f20ed1427fd61a15135bb18b26d2e75d10c0b1da
Mariana Lopez <mariana.lopez.it@gmail.com>
2022-05-03 12:16:04 -0300

    Update ingrese_nueva_palabra.js

--- ingrese_nueva_palabra.js
+++ ingrese_nueva_palabra.js
@@ -7,7 +7,7 @@ btnAgregar.onclick = agregar;
 
 
 function agregar(){
-    palabrasSecretas = ["MEXICO", "BELICE", "SUECIA", "GRECIA", "CHIPRE", "KOSOVO", "SERBIA", "MONACO", "RUSIA", "ARGENTINA",];
+    palabrasSecretas = ["MEXICO", "BELICE", "SUECIA", "GRECIA", "CHIPRE", "KOSOVO", "SERBIA", "MONACO", "RUSIA",];
     palabrasSecretas[palabrasSecretas.length] = palabraNueva.value.toUpperCase();
     localStorage.setItem("palabrasSecretas", JSON.stringify(palabrasSecretas));
     palabrasSecretasAlmacenadas = JSON.parse(localStorage.getItem("palabrasSecretas"));
